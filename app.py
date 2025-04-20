4a2e64f17650efcb4ccd681ebc6e80e6072b9660
Ranveersial <ranveersial@gmail.com>
2025-04-20 19:43:09 +0530

    Add deployment configs

--- app.py
+++ app.py
@@ -82,5 +82,8 @@ def predict():
         "corners": round(corners, 2)
     })
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
